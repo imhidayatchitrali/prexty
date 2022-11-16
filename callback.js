53c64b5a9b39466c56f13af872e7171a3d8caee2
imhidayatchitrali <hidayat.rehman64@gmail.com>
2022-11-17 01:02:06 +0500

    file name change

--- callback.js
+++ callback.js
@@ -9,16 +9,16 @@ function getPost() {
         posts.forEach((post, index) => {
             output += `<li>${post.title}</li>`
         })
-        document.body.innerHTML=output
+        document.body.innerHTML = output
     }, 1000)
 }
 
-function createPost(post) {
+function createPost(post, callBack) {
     setTimeout(() => {
         posts.push(post)
+        callBack()
     }, 2000);
 }
 
 getPost()
-
-createPost({ title: "This is post Three", body: "This is body Three" })
+createPost({ title: "This is post Three", body: "This is body Three" }, getPost)
