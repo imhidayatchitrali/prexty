f6408864d7018e08ac26b7745ad8eb12e437f7ce
imhidayatchitrali <hidayat.rehman64@gmail.com>
2022-11-17 00:57:26 +0500

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const posts = [
+    { title: "Post One", body: "This is Body One" },
+    { title: "Post two", body: "This is Body Two" }
+]
+
+function getPost() {
+    let output = ''
+    setTimeout(() => {
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML=output
+    }, 1000)
+}
+
+function createPost(post) {
+    setTimeout(() => {
+        posts.push(post)
+    }, 2000);
+}
+
+getPost()
+
+createPost({ title: "This is post Three", body: "This is body Three" })
